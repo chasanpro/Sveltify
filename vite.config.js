765b9f54bf029cb6841e78ae3a15e5857f27bea9
Chaitanya Damarasingu <chasanpro@gmail.com>
2024-06-29 14:44:44 +0530

    BC
    Added Build Compression

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+import { terser } from 'rollup-plugin-terser';
+import viteCompression from 'vite-plugin-compression';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    sourcemap: true,
-    minify: true,
-  },
+  plugins: [
+    svelte(),
+    viteCompression({ algorithm: 'brotliCompress' }), // Use gzip if preferred
+    {
+      ...terser(),
+      apply: 'build',
+    }
+  ],
   css: {
-    postcss: './postcss.config.js' // Update to postcss.config.js
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
   },
+  build: {
+    minify: 'terser',
+  }
 });
